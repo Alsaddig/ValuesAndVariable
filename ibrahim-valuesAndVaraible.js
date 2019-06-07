4525964cba2ab01288d31684b3c59026e21e5731
Alsaddig <36203347+Alsaddig@users.noreply.github.com>
2019-06-07 00:06:15 -0700

    upload values and variable file

---
+++ ibrahim-valuesAndVaraible.js
@@ -0,0 +1,29 @@
+var  firstName= "Alsaddig";
+var lastName ="Ibrahim";
+var  address ="2610 Bartelt RD Iowa City IA 52246";
+var payRate= 35.99;
+payRate.toFixed(2);
+var hireDate  = new Date(); 
+
+console.log('First Name: '+firstName+"\n"+'Last Name: '+lastName+"\n"+'Address: '
++address+"\n"+'Pay Rate: '+payRate+"\n"+'Hire Date: '+hireDate+"\n");
+
+firstName= "Adam";
+lastName ="Ibrahim";
+address ="200 Main st Iowa City IA 52246";
+payRate= 25.49;
+payRate.toFixed(2);
+hireDate  = new Date(); 
+
+console.log('First Name: '+firstName+"\n"+'Last Name: '+lastName+"\n"+'Address: '
++address+"\n"+'Pay Rate: '+payRate+"\n"+'Hire Date: '+hireDate+"\n");
+
+firstName= "Mohammed";
+lastName ="Ibrahim";
+address ="1600 2th St Iowa City IA 52246";
+payRate= 20.49;
+payRate.toFixed(2);
+hireDate  = new Date(); 
+
+console.log('First Name: '+firstName+"\n"+'Last Name: '+lastName+"\n"+'Address: '
++address+"\n"+'Pay Rate: '+payRate+"\n"+'Hire Date: '+hireDate+"\n");
